e0e8f635063a6f53334236bf0bf6ba222528283f
Nico Machado <nico.machado@gmail.com>
2024-05-28 20:25:00 -0300

    Clase POO 1.5

--- ejercicio_web.php
+++ ejercicio_web.php
@@ -1,7 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'] ?? '';
+    $apellido = $_POST['apellido'] ?? '';
     $numero = $_POST['numero'] ?? '';
+
     
     if (empty($nombre) || empty($numero)) {
         $error = "Por favor, ingrese un nombre y un número.";
@@ -21,9 +23,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php if ($_SERVER['REQUEST_METHOD'] === 'GET' || !empty($error)): ?>
         <form action="ejercicio_web.php" method="POST">
             <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" >
+            <input type="text" id="nombre" name="nombre" value="<?= $nombre ?? '' ?>" >
             <br>
-            <label for="numero">Número:</label>
+            <label for="ape">Apellido:</label>
+            <input type="text" id="apellido" name="apellido" >
+            <br>
+            <label for="numero">Número Dni:</label>
             <input type="number" id="numero" name="numero" >
             <br>
             <button type="submit">Enviar</button>
@@ -33,6 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php endif; ?>
     <?php elseif (!empty($datosIngresados)): ?>
         <p>Nombre: <?php echo htmlspecialchars($nombre); ?></p>
+        <p>Apellido: <?php echo htmlspecialchars($apellido); ?></p>
         <p>Número: <?php echo htmlspecialchars($numero); ?></p>
         <a href="ejercicio_web.php">Volver a la página inicial</a>
     <?php endif; ?>
